13a33c434f9b63ea61d90cf8cf371c41957ef17f
sgml <paul.sweatte@etrade.com>
2015-05-02 16:45:19 -0700

    Fixed pattern and message

--- console2assert.js
+++ console2assert.js
@@ -1,5 +1,8 @@
-function foo (fn)
+ function foo (fn)
    {
    console.log(window);
-   return fn.toString().replace(/log(.+)/, "assert(!!$1, 'false', $1)" )
+   return fn.toString().replace(/log.([\w]+)./, "assert(!!$1, '$1 is undefined', $1)" )
    }
+   
+var foo = foo(foo);
+var bar = Function(foo)()
